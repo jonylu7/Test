5fa89680ff726acef81e8d26b0a17b75985169b8
jonylu7 <jonylu7@gmail.com>
2023-03-08 15:53:38 +0800

    Initial Commit

---
+++ Test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Test
+//
+//  Created by 盧威任 on 3/8/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
